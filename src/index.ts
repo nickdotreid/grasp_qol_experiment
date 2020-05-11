5a432bb03e5732a1ca7837ff11a2fb60ec245156
Nick Reid <nickreid@nickreid.com>
2020-05-11 14:53:10 -0700

    Make months filterable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import { line } from 'd3';
 
 d3.csv('data/qol.csv')
 .then((data:any) => {
@@ -7,13 +8,16 @@ d3.csv('data/qol.csv')
     const ageGroups: Array<string> = [];
     const baselineGroups: Array<string> = [];
 
-    const months: Array<string> = [];
+    let currentDomain: string;
+    let viewableMonths: Array<Number>;
+
+    const months: Array<Number> = [];
+    
     data.columns.forEach((column:string) => {
         if(['Domain', 'Treatment', 'Age range', 'baseline sexual score'].indexOf(column) < 0) {
-            months.push(column);
+            months.push(Number(column));
         }
     });
-
     data.forEach((element:any) => {
         if(element['Domain'] && domains.indexOf(element['Domain']) < 0) {
             domains.push(element['Domain']);
@@ -28,6 +32,10 @@ d3.csv('data/qol.csv')
             baselineGroups.push(element['baseline sexual score']);
         }
     });
+
+    currentDomain = domains[0];
+    viewableMonths = months;
+
     d3.select('nav').selectAll('a')
     .data(domains)
     .enter()
@@ -36,59 +44,87 @@ d3.csv('data/qol.csv')
         return d;
     })
     .on('click', function(d) {
-        drawLines(d);
+        currentDomain = d;
+        drawChart(currentDomain, viewableMonths);
     });
+
+    d3.select('fieldset.months')
+    .selectAll('label')
+    .data(months)
+    .enter()
+    .append('label')
+    .text((d) => {
+        return String(d);
+    })
+    .append('input')
+    .attr('type', 'checkbox')
+    .attr('checked', 'checked')
+    .on('change', (d) => {
+        const dIndex = viewableMonths.indexOf(d);
+        if(dIndex === -1) {
+            viewableMonths.push(d);
+        } else {
+            viewableMonths.splice(dIndex, 1);
+        }
+        drawChart(currentDomain, viewableMonths);
+    })
 
     let width = 500;
     let height = 300;
     let margin = 30;
-
     const svg = d3.select('#main').append("svg")
         .attr("width", width + margin *2)
         .attr("height", height + margin *2);
-
-    const x = d3.scaleLinear()
+    let x = d3.scaleLinear()
         .domain([Number(months[0]), Number(months[months.length - 1])])
         .range([0, width]);
-    const y = d3.scaleLinear()
+
+    const xAxis = svg.append("g")
+        .attr("transform", `translate(${margin}, ${height+margin})`)
+    const yAxis = svg.append("g")
+        .attr("transform", `translate(${margin}, ${margin})`)
+    const chart = svg.append("g")
+        .attr('transform', `translate(${margin}, ${margin})`);
+
+    function drawChart(domain: string, _months: Array<Number>) {
+        const minMonth = Number(d3.min(_months));
+        const maxMonth = Number(d3.max(_months));
+
+        const x = d3.scaleLinear()
+        .domain([minMonth, maxMonth])
+        .range([0, width]);
+        const y = d3.scaleLinear()
         .domain([100, 0])
         .range([0,height]);
-
-    svg.append("g")
-        .attr("transform", `translate(${margin}, ${height+margin})`)
-        .call(d3.axisBottom(x));
-    svg.append("g")
-        .attr("transform", `translate(${margin}, ${margin})`)
-        .call(d3.axisLeft(y));
-
-    const lineGenerator = d3.line<any>()
-        .x((d, i) => {
+        const lineGenerator = d3.line<any>()
+        .x((d) => {
             return x(d['month']);
         })
         .y((d) => {
             return y(d['value']);
         });
 
-    function drawLines(domain: string) {
-        let lines = data.filter((d:any) => {
+        xAxis.call(d3.axisBottom(x));
+        yAxis.call(d3.axisLeft(y));
+
+        const lines = data.filter((d:any) => {
             return d['Domain'] == domain;
-        });
-        lines = lines.map((d:any) => {
+        }).map((d:any) => {
             return months.map((month) => {
                 return {
-                    'value': d[month],
-                    'month': Number(month),
+                    'value': d[String(month)],
+                    'month': month,
                     'domain': d['Domain'],
                     'age': d['Age range']
                 };
             });
+        })
+        .map((d:any) => {
+            return lineGenerator(d);
         });
-        console.log(lines);
-        
-        console.log(lines[0]);
 
-        const chart = svg.append("g")
-            .attr('transform', `translate(${margin}, ${margin})`);
+        chart.selectAll("path")
+        .remove();
 
         chart.selectAll("path")
             .data(lines)
@@ -98,11 +134,11 @@ d3.csv('data/qol.csv')
             .attr("stroke", "steelblue")
             .attr("stroke-width", 1.5)
             .attr("d", (d) => {
-                return lineGenerator(d);
+                return d;
             });
     }
 
-    drawLines(domains[0]);
+    drawChart(currentDomain, viewableMonths);
 
 });
 
